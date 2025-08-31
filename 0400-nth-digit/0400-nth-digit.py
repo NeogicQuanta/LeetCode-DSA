c5040b916cf3a8ac865e5df38143062fef9123fb
Eshan Rathore <176816991+NeogicQuanta@users.noreply.github.com>
2025-08-31 12:18:15 +0530

    Time: 0 ms (100%), Space: 17.8 MB (64.1%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.py
+++ 0400-nth-digit/0400-nth-digit.py
@@ -3,27 +3,17 @@ class Solution:
         if n ==1:
             return 1
         count = 1
-        # l = len(str(n))
-        # for i in range(1, l):
-        #     count += i*(10**i - 10**(i-1))
-        #     print(i, count)
-        # # Reached the Base Point
         i = 0
         while count < n:
             count += int(i*(10**i - 10**(i-1)))
             i += 1
-            print(" ", i, count)
         i -= 1
         count -= i*(10**i - 10**(i-1))
+        
         number = 10**(i-1)
-        print("\t",i, count, number)
         temp = n - count
-        print(temp)
         number += (temp)//i
         count += i*(temp//i)
-        print(number, count)
-
-
 
         number = str(number)
         for j in number:

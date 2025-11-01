6183e6499cd4495509b9b0aeae8e4dbda3879ec6
Eshan Rathore <176816991+NeogicQuanta@users.noreply.github.com>
2025-11-01 11:39:49 +0530

    Time: 0 ms (100%), Space: 20.2 MB (56.21%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int countNodes(struct TreeNode* root) {
+    if (root) return 1 + countNodes(root->left) + countNodes(root->right);
+    return 0;
+}
